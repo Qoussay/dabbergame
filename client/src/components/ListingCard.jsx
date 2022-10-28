d5a26da91124f30d9fea9eb619670fc39e45e0ab
Qoussay <qoussay.akkari@gmail.com>
2022-10-28 21:18:43 +0800

    added the listingCard where each listing will be displayed. I used a mockdata which should represent the data that will be received from the api

---
+++ client/src/components/ListingCard.jsx
@@ -0,0 +1,65 @@
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const platformData = [
+  {
+    name: "playstation5",
+    color: "bg-white",
+    text: "text-black",
+  },
+  {
+    name: "playstation4",
+    color: "bg-blue-700",
+    text: "text-white",
+  },
+];
+export default function ListingCard(props) {
+  let bannerColor = "";
+  let bannerTextColor = "";
+
+  switch (props.data.platform) {
+    case "playstation5":
+      bannerColor = "bg-white";
+      bannerTextColor = "text-black";
+      break;
+    case "playstation4":
+      bannerColor = "bg-blue-700";
+      bannerTextColor = "text-white";
+      break;
+    case "xboxOne":
+      bannerColor = "bg-green-500";
+      bannerTextColor = "text-white";
+      break;
+    default:
+      bannerColor = "bg-black";
+      bannerTextColor = "text-black";
+      break;
+  }
+  return (
+    <div className="flex flex-col shadow-md shadow-bg-dark">
+      <div
+        className={`${bannerColor} ${bannerTextColor} text-sm text-center rounded-t-lg`}
+      >
+        {props.data.platform}
+      </div>
+      <img src={props.data.coverURL}></img>
+      <div className="bg-bg-light rounded-b-lg px-2 py-1 flex flex-row">
+        <div className="flex flex-col justify-center">
+          <FontAwesomeIcon
+            icon={faUser}
+            className="text-bg-dark pr-2 text-xl"
+          />
+        </div>
+        <div className="flex flex-col text-xs">
+          <p className="text-text-white">{props.data.user}</p>
+          <p className=" text-text-light">{props.data.state}</p>
+        </div>
+        <div className="flex flex-col justify-center w-1/2">
+          <h1 className=" text-lg text-accent text-right">
+            {props.data.price} TND
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/ListingsViewer.jsx
+++ client/src/components/ListingsViewer.jsx
@@ -1,7 +1,52 @@
+import Select from "react-select";
+import data from "../mock/listings.json";
+import ListingCard from "./ListingCard";
+
+const sortOptions = [
+  { value: "option1", label: "option1" },
+  { value: "option2", label: "option2" },
+  { value: "option3", label: "option3" },
+  { value: "option4", label: "option4" },
+  { value: "option5", label: "option5" },
+];
+
 export default function ListingsViewer() {
+  console.log(data);
   return (
-    <div>
-      <h1>Yoo Listings Viewer</h1>
+    <div className="py-6">
+      {/* filters and sort */}
+      <form className="flex flex-row space-x-5">
+        <Select
+          options={sortOptions}
+          placeholder="Sort By:"
+          className="w-1/6 rounded-lg"
+        />
+
+        <p className="text-3xl text-text-white">|</p>
+        <Select
+          options={sortOptions}
+          placeholder="Platform:"
+          className="w-[12%] rounded-lg"
+        />
+        <Select
+          options={sortOptions}
+          placeholder="State:"
+          className="w-[12%] rounded-lg"
+        />
+        <Select
+          options={sortOptions}
+          placeholder="Condition:"
+          className="w-[12%] rounded-lg"
+        />
+      </form>
+      <div className="grid grid-cols-6 gap-6 py-8">
+        {data.map((listing) => {
+          return <ListingCard data={listing} />;
+        })}
+      </div>
     </div>
   );
 }
+
+// TO-DO:
+// modify the Select tag
