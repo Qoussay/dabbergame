2e851a6c8493eb42f7488304c3bb2578bf15433e
Qoussay <qoussay.akkari@gmail.com>
2022-11-01 05:18:55 +0800

    created the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
