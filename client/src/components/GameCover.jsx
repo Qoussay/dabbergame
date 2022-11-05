6c7af9e55cfc1c42e1fd59dfc62c46797f843338
Qoussay <qoussay.akkari@gmail.com>
2022-11-05 20:39:43 +0800

    some tweaks here and there are missing. Need to change the cover image to be according to the one chosen by the user. Also swill missing the games to be traded with section

--- client/src/components/GameCover.jsx
+++ client/src/components/GameCover.jsx
@@ -7,7 +7,11 @@ export default function GameCover({
   className,
 }) {
   return (
-    <div className={`${className} flex flex-col font-bold`}>
+    <div
+      className={`${className} flex flex-col font-bold ${
+        rounded ? "rounded-b-lg" : null
+      }`}
+    >
       <PlatformBanner platform={platform} textSize={textSize} />
       <img src={url} className={rounded ? "rounded-b-lg" : null}></img>
     </div>
