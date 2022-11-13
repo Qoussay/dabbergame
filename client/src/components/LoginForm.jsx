2e107d4fd4ecc5387382a065a7c2704115af5a71
Qoussay <qoussay.akkari@gmail.com>
2022-11-13 13:54:48 +0800

    added facebook and google icons to the login modal

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,14 +1,16 @@
 import Button from "./Button";
 import { TextField } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
 
 export default function LoginForm({ onClick }) {
   return (
-    <div>
+    <div className="px-4">
       <h1 className="text-center text-text-white text-2xl text py-5 font-semibold ">
         Login
       </h1>
 
-      <form className="grow flex flex-col justify-center px-4 space-y-4">
+      <form className="grow flex flex-col justify-center space-y-4">
         <TextField
           sx={{
             background: "#fff",
@@ -41,6 +43,21 @@ export default function LoginForm({ onClick }) {
           className="mb-5 mt-3 py-3 hover:brightness-110"
         />
       </form>
+
+      <div className="flex flex-row place-items-center text-text-white space-x-3 text-lg">
+        <hr className="grow" />
+        <div>Log in with</div>
+        <hr className="grow" />
+      </div>
+
+      <div className="flex flex-row justify-center space-x-4 py-3">
+        <div className="rounded-lg bg-white drop-shadow hover:drop-shadow-lg hover:brightness-90 hover:cursor-pointer hover:scale-105 transition-all duration-300">
+          <FacebookIcon fontSize="large" htmlColor="#156EE5" />
+        </div>
+        <div className="rounded-lg bg-[#EA4335] drop-shadow hover:drop-shadow-lg hover:brightness-90 hover:cursor-pointer hover:scale-105 transition-all duration-300">
+          <GoogleIcon fontSize="large" htmlColor="#fff" />
+        </div>
+      </div>
 
       <div className="flex flex-row justify-center py-2 space-x-2">
         <p className="text-text-white flex flex-col justify-center">
