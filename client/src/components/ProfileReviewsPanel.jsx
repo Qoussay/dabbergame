876c64b06842ef9b0c45fb2128ab3b5cf314819d
Qoussay <qoussay.akkari@gmail.com>
2022-11-21 03:28:08 +0800

    hotfix

--- client/src/components/ProfileReviewsPanel.jsx
+++ client/src/components/ProfileReviewsPanel.jsx
@@ -33,20 +33,62 @@ export default function ProfileReviewsPanel({ reviews, user }) {
   };
   const navigate = useNavigate();
 
-  if (reviews.length === 0) {
-    return (
-      <div>
-        <ReviewModal
-          open={openReviewModal}
-          closeButtonClick={() => handleReviewModalClose()}
-          target={user.username}
-          source={loggedUser}
-        />
+  // if (reviews.length === 0) {
+  //   return (
+  //     <div>
+  //       <ReviewModal
+  //         open={openReviewModal}
+  //         closeButtonClick={() => handleReviewModalClose()}
+  //         target={user.username}
+  //         source={loggedUser}
+  //       />
 
+  //       <div className=" flex flex-row">
+  //         <div className="grow text-lg text-text-white">
+  //           The user has no reviews yet
+  //         </div>
+  //         {loggedUser === user.username ? null : (
+  //           <Button
+  //             text="Add a review"
+  //             bgColor="bg-accent"
+  //             textColor="text-text-dark"
+  //             icon={
+  //               <FontAwesomeIcon
+  //                 icon={faSquarePlus}
+  //                 className="text-text-dark pr-2"
+  //               />
+  //             }
+  //             className="text-base py-2"
+  //             onClick={handleReviewModalOpen}
+  //           />
+  //         )}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+  return (
+    <div>
+      <ReviewModal
+        open={openReviewModal}
+        closeButtonClick={() => handleReviewModalClose()}
+        target={user.username}
+        source={loggedUser}
+      />
+      <div className="flex flex-col space-y-4">
         <div className=" flex flex-row">
-          <div className="grow text-lg text-text-white">
-            The user has no reviews yet
-          </div>
+          {reviews.length === 0 ? (
+            <div className="grow text-lg text-text-white">
+              The user has no reviews yet
+            </div>
+          ) : (
+            <div className="flex flex-col grow text-lg text-text-white z-0">
+              <UserReviewsScore username={user.username} />
+              <div className="text-text-light">
+                {reviews.length} {reviews.length > 1 ? "Reviews" : "Review"}
+              </div>
+            </div>
+          )}
+
           {loggedUser === user.username ? null : (
             <Button
               text="Add a review"
@@ -62,39 +104,6 @@ export default function ProfileReviewsPanel({ reviews, user }) {
               onClick={handleReviewModalOpen}
             />
           )}
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <ReviewModal
-        open={openReviewModal}
-        closeButtonClick={() => handleReviewModalClose()}
-        target={user.username}
-        source={loggedUser}
-      />
-      <div className="flex flex-col space-y-4">
-        <div className=" flex flex-row">
-          <div className="flex flex-col grow text-lg text-text-white z-0">
-            <UserReviewsScore username={user.username} />
-            <div className="text-text-light">
-              {reviews.length} {reviews.length > 1 ? "Reviews" : "Review"}
-            </div>
-          </div>
-          <Button
-            text="Add a review"
-            bgColor="bg-accent"
-            textColor="text-text-dark"
-            icon={
-              <FontAwesomeIcon
-                icon={faSquarePlus}
-                className="text-text-dark pr-2"
-              />
-            }
-            className="text-base py-2"
-            onClick={handleReviewModalOpen}
-          />
         </div>
         {reviews.map((review) => {
           return (
